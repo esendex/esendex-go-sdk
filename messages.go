f21dfcb7d07288d7126f20be7a11071a20851ad4
Joshua Hawxwell <m@hawx.me>
2015-07-17 10:17:34 +0100

    Add reading inbox messages

--- messages.go
+++ messages.go
@@ -33,6 +33,27 @@ type MessageResponse struct {
 	Parts        int
 	Username     string
 }
+type MessagesReceivedResponse struct {
+	Paging
+	Messages []MessageReceivedResponse
+}
+
+type MessageReceivedResponse struct {
+	Id         string
+	Uri        string
+	Reference  string
+	Status     string
+	ReceivedAt time.Time
+	Type       string
+	To         string
+	From       string
+	Summary    string
+	BodyUri    string
+	Direction  string
+	Parts      int
+	ReadAt     time.Time
+	ReadBy     string
+}
 
 type MessagesClient struct {
 	*Client
@@ -83,6 +104,57 @@ func (c *MessagesClient) Sent(opts ...Option) (*MessagesResponse, error) {
 			Direction:    message.Direction,
 			Parts:        message.Parts,
 			Username:     message.Username,
+		}
+	}
+
+	return response, nil
+}
+
+func (c *MessagesClient) Received(opts ...Option) (*MessagesReceivedResponse, error) {
+	req, err := c.NewRequest("GET", "/v1.0/inbox/messages", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, opt := range opts {
+		opt(req)
+	}
+
+	var v inboxResponse
+	resp, err := c.Do(req, &v)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("Expected 200")
+	}
+
+	response := &MessagesReceivedResponse{
+		Paging: Paging{
+			StartIndex: v.StartIndex,
+			Count:      v.Count,
+			TotalCount: v.TotalCount,
+		},
+		Messages: make([]MessageReceivedResponse, len(v.Messages)),
+	}
+
+	for i, message := range v.Messages {
+		response.Messages[i] = MessageReceivedResponse{
+			Id:         message.Id,
+			Uri:        message.Uri,
+			Reference:  message.Reference,
+			Status:     message.Status,
+			ReceivedAt: message.ReceivedAt.Time,
+			Type:       message.Type,
+			To:         message.To,
+			From:       message.From,
+			Summary:    message.Summary,
+			BodyUri:    message.Body.Uri,
+			Direction:  message.Direction,
+			Parts:      message.Parts,
+			ReadAt:     message.ReadAt.Time,
+			ReadBy:     message.ReadBy,
 		}
 	}
 
@@ -150,6 +222,33 @@ type messageHeadersResponseMessageHeader struct {
 	Direction string `xml:"direction"`
 	Parts     int    `xml:"parts"`
 	Username  string `xml:"username"`
+}
+
+type inboxResponse struct {
+	XMLName    xml.Name                     `xml:"http://api.esendex.com/ns/ messageheaders"`
+	StartIndex int                          `xml:"startindex,attr"`
+	Count      int                          `xml:"count,attr"`
+	TotalCount int                          `xml:"totalcount,attr"`
+	Messages   []inboxResponseMessageHeader `xml:"messageheader"`
+}
+
+type inboxResponseMessageHeader struct {
+	Id         string            `xml:"id,attr"`
+	Uri        string            `xml:"uri,attr"`
+	Reference  string            `xml:"reference"`
+	Status     string            `xml:"status"`
+	ReceivedAt messageHeaderTime `xml:"receivedat"`
+	Type       string            `xml:"type"`
+	To         string            `xml:"to>phonenumber"`
+	From       string            `xml:"from>phonenumber"`
+	Summary    string            `xml:"summary"`
+	Body       struct {
+		Uri string `xml:"uri,attr"`
+	} `xml:"body"`
+	Direction string            `xml:"direction"`
+	Parts     int               `xml:"parts"`
+	ReadAt    messageHeaderTime `xml:"readat"`
+	ReadBy    string            `xml:"readby"`
 }
 
 const messageHeaderTimeFormat = "2006-01-02T15:04:05.999999999"
--- messages_test.go
+++ messages_test.go
@@ -269,3 +269,189 @@ func TestMessagesById(t *testing.T) {
 	assert.Equal(parts, result.Parts)
 	assert.Equal(username, result.Username)
 }
+
+func TestMessagesReceived(t *testing.T) {
+	const (
+		startIndex  = 0
+		count       = 15
+		totalCount  = 200
+		id          = "messageheaderid"
+		uri         = "http://somemessageheader"
+		reference   = "EXETRTRE"
+		status      = "STATUS"
+		messageType = "TYPE"
+		to          = "4538224364236"
+		from        = "428377843"
+		summary     = "SUM"
+		bodyUri     = "http://rrehekr"
+		direction   = "OUT"
+		parts       = 1
+		readBy      = "someone"
+	)
+
+	var (
+		receivedAt    = time.Date(2012, 1, 1, 12, 0, 5, 0, time.UTC)
+		receivedAtStr = "2012-01-01T12:00:05"
+		readAt        = time.Date(2012, 1, 1, 12, 0, 2, 0, time.UTC)
+		readAtStr     = "2012-01-01T12:00:02"
+	)
+
+	h := newRecordingHandler(`<?xml version="1.0" encoding="utf-8"?>
+<messageheaders startindex="`+strconv.Itoa(startIndex)+`" count="`+strconv.Itoa(count)+`" totalcount="`+strconv.Itoa(totalCount)+`" xmlns="http://api.esendex.com/ns/">
+ <messageheader id="`+id+`" uri="`+uri+`">
+  <reference>`+reference+`</reference>
+  <status>`+status+`</status>
+  <receivedat>`+receivedAtStr+`</receivedat>
+  <type>`+messageType+`</type>
+  <to>
+    <phonenumber>`+to+`</phonenumber>
+  </to>
+  <from>
+   <phonenumber>`+from+`</phonenumber>
+  </from>
+  <summary>`+summary+`</summary>
+  <body uri="`+bodyUri+`"/>
+  <direction>`+direction+`</direction>
+  <parts>`+strconv.Itoa(parts)+`</parts>
+  <readat>`+readAtStr+`</readat>
+  <readby>`+readBy+`</readby>
+ </messageheader>
+</messageheaders>`, 200, map[string]string{})
+	s := httptest.NewServer(h)
+	defer s.Close()
+
+	client := xesende.New("user", "pass")
+	client.BaseUrl, _ = url.Parse(s.URL)
+
+	result, err := client.Messages.Received()
+
+	assert := assert.New(t)
+
+	assert.Nil(err)
+
+	assert.Equal("GET", h.Request.Method)
+	assert.Equal("/v1.0/inbox/messages", h.Request.URL.String())
+
+	if user, pass, ok := h.Request.BasicAuth(); assert.True(ok) {
+		assert.Equal("user", user)
+		assert.Equal("pass", pass)
+	}
+
+	assert.Equal(startIndex, result.StartIndex)
+	assert.Equal(count, result.Count)
+	assert.Equal(totalCount, result.TotalCount)
+
+	if assert.Equal(1, len(result.Messages)) {
+		message := result.Messages[0]
+
+		assert.Equal(id, message.Id)
+		assert.Equal(uri, message.Uri)
+		assert.Equal(reference, message.Reference)
+		assert.Equal(status, message.Status)
+		assert.Equal(receivedAt, message.ReceivedAt)
+		assert.Equal(messageType, message.Type)
+		assert.Equal(to, message.To)
+		assert.Equal(from, message.From)
+		assert.Equal(summary, message.Summary)
+		assert.Equal(bodyUri, message.BodyUri)
+		assert.Equal(direction, message.Direction)
+		assert.Equal(parts, message.Parts)
+		assert.Equal(readAt, message.ReadAt)
+		assert.Equal(readBy, message.ReadBy)
+	}
+}
+
+func TestMessagesReceivedWithPaging(t *testing.T) {
+	const (
+		startIndex  = 0
+		count       = 15
+		totalCount  = 200
+		id          = "messageheaderid"
+		uri         = "http://somemessageheader"
+		reference   = "EXETRTRE"
+		status      = "STATUS"
+		messageType = "TYPE"
+		to          = "4538224364236"
+		from        = "428377843"
+		summary     = "SUM"
+		bodyUri     = "http://rrehekr"
+		direction   = "OUT"
+		parts       = 1
+		readBy      = "someone"
+	)
+
+	var (
+		receivedAt    = time.Date(2012, 1, 1, 12, 0, 5, 0, time.UTC)
+		receivedAtStr = "2012-01-01T12:00:05"
+		readAt        = time.Date(2012, 1, 1, 12, 0, 2, 0, time.UTC)
+		readAtStr     = "2012-01-01T12:00:02"
+	)
+
+	h := newRecordingHandler(`<?xml version="1.0" encoding="utf-8"?>
+<messageheaders startindex="`+strconv.Itoa(startIndex)+`" count="`+strconv.Itoa(count)+`" totalcount="`+strconv.Itoa(totalCount)+`" xmlns="http://api.esendex.com/ns/">
+ <messageheader id="`+id+`" uri="`+uri+`">
+  <reference>`+reference+`</reference>
+  <status>`+status+`</status>
+  <receivedat>`+receivedAtStr+`</receivedat>
+  <type>`+messageType+`</type>
+  <to>
+    <phonenumber>`+to+`</phonenumber>
+  </to>
+  <from>
+   <phonenumber>`+from+`</phonenumber>
+  </from>
+  <summary>`+summary+`</summary>
+  <body uri="`+bodyUri+`"/>
+  <direction>`+direction+`</direction>
+  <parts>`+strconv.Itoa(parts)+`</parts>
+  <readat>`+readAtStr+`</readat>
+  <readby>`+readBy+`</readby>
+ </messageheader>
+</messageheaders>`, 200, map[string]string{})
+	s := httptest.NewServer(h)
+	defer s.Close()
+
+	client := xesende.New("user", "pass")
+	client.BaseUrl, _ = url.Parse(s.URL)
+
+	result, err := client.Messages.Received(xesende.Page(5, 10))
+
+	assert := assert.New(t)
+
+	assert.Nil(err)
+
+	assert.Equal("GET", h.Request.Method)
+	assert.Equal("/v1.0/inbox/messages", h.Request.URL.Path)
+
+	if user, pass, ok := h.Request.BasicAuth(); assert.True(ok) {
+		assert.Equal("user", user)
+		assert.Equal("pass", pass)
+	}
+
+	query := h.Request.URL.Query()
+	assert.Equal("5", query.Get("startindex"))
+	assert.Equal("10", query.Get("count"))
+
+	assert.Equal(startIndex, result.StartIndex)
+	assert.Equal(count, result.Count)
+	assert.Equal(totalCount, result.TotalCount)
+
+	if assert.Equal(1, len(result.Messages)) {
+		message := result.Messages[0]
+
+		assert.Equal(id, message.Id)
+		assert.Equal(uri, message.Uri)
+		assert.Equal(reference, message.Reference)
+		assert.Equal(status, message.Status)
+		assert.Equal(receivedAt, message.ReceivedAt)
+		assert.Equal(messageType, message.Type)
+		assert.Equal(to, message.To)
+		assert.Equal(from, message.From)
+		assert.Equal(summary, message.Summary)
+		assert.Equal(bodyUri, message.BodyUri)
+		assert.Equal(direction, message.Direction)
+		assert.Equal(parts, message.Parts)
+		assert.Equal(readAt, message.ReadAt)
+		assert.Equal(readBy, message.ReadBy)
+	}
+}
