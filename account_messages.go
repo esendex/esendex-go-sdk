a987760fb09845c91966dc1c187258b549c40dbd
Joshua Hawxwell <m@hawx.me>
2015-07-17 11:31:24 +0100

    Add account based sent message retrieval

--- account_messages.go
+++ account_messages.go
@@ -8,6 +8,58 @@ type AccountMessagesClient struct {
 	*AccountClient
 }
 
+// Sent returns a list of messages sent by the account.
+func (c *AccountMessagesClient) Sent(opts ...Option) (*MessagesResponse, error) {
+	req, err := c.newRequest("GET", "/v1.0/messageheaders?accountReference="+c.reference, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, opt := range opts {
+		opt(req)
+	}
+
+	var v messageHeadersResponse
+	resp, err := c.do(req, &v)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("Expected 200")
+	}
+
+	response := &MessagesResponse{
+		Paging: Paging{
+			StartIndex: v.StartIndex,
+			Count:      v.Count,
+			TotalCount: v.TotalCount,
+		},
+		Messages: make([]MessageResponse, len(v.Messages)),
+	}
+
+	for i, message := range v.Messages {
+		response.Messages[i] = MessageResponse{
+			Id:           message.Id,
+			Uri:          message.Uri,
+			Reference:    message.Reference,
+			Status:       message.Status,
+			LastStatusAt: message.LastStatusAt.Time,
+			SubmittedAt:  message.SubmittedAt.Time,
+			Type:         message.Type,
+			To:           message.To,
+			From:         message.From,
+			Summary:      message.Summary,
+			BodyUri:      message.Body.Uri,
+			Direction:    message.Direction,
+			Parts:        message.Parts,
+			Username:     message.Username,
+		}
+	}
+
+	return response, nil
+}
+
 // Received returns the messages sent to the account.
 func (c *AccountMessagesClient) Received(opts ...Option) (*MessagesReceivedResponse, error) {
 	req, err := c.newRequest("GET", "/v1.0/inbox/"+c.reference+"/messages", nil)
--- account_messages_test.go
+++ account_messages_test.go
@@ -11,6 +11,102 @@ import (
 	"hawx.me/code/xesende"
 )
 
+func TestAccountMessagesSent(t *testing.T) {
+	const (
+		startIndex  = 0
+		count       = 15
+		totalCount  = 200
+		id          = "messageheaderid"
+		uri         = "http://somemessageheader"
+		reference   = "EXETRTRE"
+		status      = "STATUS"
+		messageType = "TYPE"
+		to          = "4538224364236"
+		from        = "428377843"
+		summary     = "SUM"
+		bodyUri     = "http://rrehekr"
+		direction   = "OUT"
+		parts       = 1
+		username    = "user"
+	)
+
+	var (
+		lastStatusAt    = time.Date(2012, 1, 1, 12, 0, 5, 0, time.UTC)
+		lastStatusAtStr = "2012-01-01T12:00:05.000"
+		submittedAt     = time.Date(2012, 1, 1, 12, 0, 2, 0, time.UTC)
+		submittedAtStr  = "2012-01-01T12:00:02.000"
+	)
+
+	h := newRecordingHandler(`<?xml version="1.0" encoding="utf-8"?>
+<messageheaders startindex="`+strconv.Itoa(startIndex)+`" count="`+strconv.Itoa(count)+`" totalcount="`+strconv.Itoa(totalCount)+`" xmlns="http://api.esendex.com/ns/">
+ <messageheader id="`+id+`" uri="`+uri+`">
+  <reference>`+reference+`</reference>
+  <status>`+status+`</status>
+  <laststatusat>`+lastStatusAtStr+`</laststatusat>
+  <submittedat>`+submittedAtStr+`</submittedat>
+  <type>`+messageType+`</type>
+  <to>
+    <phonenumber>`+to+`</phonenumber>
+  </to>
+  <from>
+   <phonenumber>`+from+`</phonenumber>
+  </from>
+  <summary>`+summary+`</summary>
+  <body uri="`+bodyUri+`"/>
+  <direction>`+direction+`</direction>
+  <parts>`+strconv.Itoa(parts)+`</parts>
+  <username>`+username+`</username>
+ </messageheader>
+</messageheaders>`, 200, map[string]string{})
+	s := httptest.NewServer(h)
+	defer s.Close()
+
+	client := xesende.New("user", "pass")
+	client.BaseUrl, _ = url.Parse(s.URL)
+
+	account := client.Account("EXHEY")
+
+	result, err := account.Messages.Sent()
+
+	assert := assert.New(t)
+
+	assert.Nil(err)
+
+	assert.Equal("GET", h.Request.Method)
+	assert.Equal("/v1.0/messageheaders", h.Request.URL.Path)
+
+	if user, pass, ok := h.Request.BasicAuth(); assert.True(ok) {
+		assert.Equal("user", user)
+		assert.Equal("pass", pass)
+	}
+
+	query := h.Request.URL.Query()
+	assert.Equal("EXHEY", query.Get("accountReference"))
+
+	assert.Equal(startIndex, result.StartIndex)
+	assert.Equal(count, result.Count)
+	assert.Equal(totalCount, result.TotalCount)
+
+	if assert.Equal(1, len(result.Messages)) {
+		message := result.Messages[0]
+
+		assert.Equal(id, message.Id)
+		assert.Equal(uri, message.Uri)
+		assert.Equal(reference, message.Reference)
+		assert.Equal(status, message.Status)
+		assert.Equal(lastStatusAt, message.LastStatusAt)
+		assert.Equal(submittedAt, message.SubmittedAt)
+		assert.Equal(messageType, message.Type)
+		assert.Equal(to, message.To)
+		assert.Equal(from, message.From)
+		assert.Equal(summary, message.Summary)
+		assert.Equal(bodyUri, message.BodyUri)
+		assert.Equal(direction, message.Direction)
+		assert.Equal(parts, message.Parts)
+		assert.Equal(username, message.Username)
+	}
+}
+
 func TestAccountMessagesReceived(t *testing.T) {
 	const (
 		startIndex  = 0
